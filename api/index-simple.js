62cd3409de04e4d283638f2b1f7a9aeddc0d7e5a
shamyt9 <161078949+shamyt9@users.noreply.github.com>
2026-05-03 17:37:41 +0530

    Remove conflicting handler files

--- api/index-simple.js
+++
@@ -1,7 +0,0 @@
-const app = require('../frontend/api/dist/app').default;
-
-// Simple pass-through handler - don't try to manage DB in serverless
-module.exports = (req, res) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}`);
-    return app(req, res);
-};
